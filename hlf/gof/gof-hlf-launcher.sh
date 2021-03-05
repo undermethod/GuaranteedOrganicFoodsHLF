f7cb9c44f46c9ee8fd802318e081fff47a890b3a
undermethod <undermethod@gmail.com>
2021-03-05 21:36:07 +0000

    updated user roles for GOF

--- hlf/gof/gof-hlf-launcher.sh
+++ hlf/gof/gof-hlf-launcher.sh
@@ -3,9 +3,14 @@
 # Exit on first error
 set -e
 
+cd ../chaincode/gof/javascript
+npm i
+cd ../../../gof/javascript
+npm i
+cd ..
 ./startFabric.sh javascript
 cd javascript
 node enrollAdmin
-node registerUser
-node invoke
+node registerUsersGOF
+node invokeGOF
 node queryGOF
--- hlf/gof/javascript/queryGOF.js
+++ hlf/gof/javascript/queryGOF.js
@@ -21,17 +21,19 @@ async function main() {
         const wallet = await Wallets.newFileSystemWallet(walletPath);
         console.log(`Wallet path: ${walletPath}`);
 
+        const queryUser = 'producer';
+
         // Check to see if we've already enrolled the user.
-        const identity = await wallet.get('appUser');
+        const identity = await wallet.get(queryUser);
         if (!identity) {
-            console.log('An identity for the user "appUser" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
+            console.log(`An identity for the user "${queryUser}" does not exist in the wallet`);
+            console.log('Run the registerUsersGOF.js application before retrying');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: queryUser, discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('gofchannel');
