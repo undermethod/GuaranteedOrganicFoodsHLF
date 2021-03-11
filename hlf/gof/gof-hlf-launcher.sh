495aefc8cd43fd5cf5362af625f29577e7988637
undermethod <undermethod@gmail.com>
2021-03-11 23:39:26 +0000

    updated launcher for rm node_modules before install

--- hlf/gof/gof-hlf-launcher.sh
+++ hlf/gof/gof-hlf-launcher.sh
@@ -7,8 +7,10 @@ gofd=$(pwd)
 cd ~
 curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.2.2
 cd $gofd/../chaincode/gof/javascript
+rm -rf node_modules package-lock.json
 npm i
 cd ../../../gof/javascript
+rm -rf node_modules package-lock.json
 npm i
 cd ..
 ./startFabric.sh javascript
